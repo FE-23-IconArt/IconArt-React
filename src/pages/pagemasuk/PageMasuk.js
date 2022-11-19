7797e9eccb83134cf4e24814ef802b5f626a5ed6
aqillanl <aqilla.x.rpl3@gmail.com>
2022-11-19 21:36:15 +0700

    ubah

--- src/pages/pagemasuk/PageMasuk.js
+++ src/pages/pagemasuk/PageMasuk.js
@@ -1,44 +1,43 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Signin } from "../../redux/actions/UserActions";
-import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Signin } from "../../redux/actions/UserActions";
+// import { useState } from "react";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
 const PageMasuk = () => {
-  const { user } = useSelector((state) => state.user);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  // const { user } = useSelector((state) => state.user);
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    dispatch(Signin([email], [password]));
-  });
+  // useEffect(() => {
+  //   dispatch(Signin([email], [password]));
+  // });
 
-  const SubmitForm = (e) => {
-    e.preventDefault();
-    const cariAkun = user.find((item) => item.email === email && item.password === password);
+  // const SubmitForm = (e) => {
+  //   e.preventDefault();
+  //   const cariAkun = user.find((item) => item.email === email && item.password === password);
 
-    if (cariAkun) {
-      localStorage.setItem("user", JSON.stringify(cariAkun));
-      toast.success(" Selamat kamu berhasil login!", {
-        position: toast.POSITION.TOP_RIGHT,
-      });
-      navigate("/");
-    } else {
-      toast.error("kamu belum punya akun", {
-        position: toast.POSITION.TOP_RIGHT,
-      });
-    }
-  };
+  //   if (cariAkun) {
+  //     localStorage.setItem("user", JSON.stringify(cariAkun));
+  //     toast.success(" Selamat kamu berhasil login!", {
+  //       position: toast.POSITION.TOP_RIGHT,
+  //     });
+  //     navigate("/");
+  //   } else {
+  //     toast.error("kamu belum punya akun", {
+  //       position: toast.POSITION.TOP_RIGHT,
+  //     });
+  //   }
+  // };
   return (
     <>
       <section className="main py-3">
         <div className="container">
-          <ToastContainer />
           <div className="row">
             <div className="col text-center pt-5">
               <h2>Welcome Back</h2>
@@ -49,15 +48,15 @@ const PageMasuk = () => {
             <div className="row">
               <div className="col-md-4 offset-md-4">
                 <div className="login-form mt-4 p-4">
-                  <form id="formSignIn" className="row g-3" method="GET" onSubmit={SubmitForm}>
+                  <form id="formSignIn" className="row g-3" method="GET">
                     <h4 className="text-center">Sign In</h4>
                     <div className="col-12">
                       <label>Email</label>
-                      <input type="text" id="username" name="username" className="form-control" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                      <input type="text" id="username" name="username" className="form-control" placeholder="Email" />
                     </div>
                     <div className="col-12">
                       <label>Password</label>
-                      <input type="password" id="password" name="password" className="form-control" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                      <input type="password" id="password" name="password" className="form-control" placeholder="Password" />
                     </div>
                     <div className="col-12">
                       <button type="submit" className="btn btn-primary">
